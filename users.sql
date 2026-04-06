116602a7a30250e06eda0763c3cf5f4fee7c4d12
unknown <fauzibspp@gmail.com>
2026-04-06 09:21:45 +0800

    updated users.sql

--- users.sql
+++ users.sql
@@ -3,10 +3,7 @@ CREATE TABLE users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    roles VARCHAR(50) NOT NULL DEFAULT 'sekolah',
-    jpn_id BIGINT NULL,
-    ppd_id BIGINT NULL,
-    state VARCHAR(100) NULL,
+    roles VARCHAR(50) NOT NULL DEFAULT 'admin',
     created_at DATETIME NULL,
     updated_at DATETIME NULL
 );
